fe59053578da3f8b61e12b26920219edb32f23ac
Saull Brandão <saull@outlook.com.com>
2021-11-17 14:52:23 -0300

    feat: subscribe using stripe payment and store subscription info to the db

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 type SubscribeButtonProps = {
@@ -5,5 +8,28 @@ type SubscribeButtonProps = {
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
-  return <button className={styles.subscribeButton}>Subscribe now</button>
+  const [session] = useSession()
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
+  return (
+    <button onClick={handleSubscribe} className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  )
 }
