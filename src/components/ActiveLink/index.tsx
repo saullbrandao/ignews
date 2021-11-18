fad1c301dca2579bf234f246942ace78138236cc
Saull Brandão <saull@outlook.com.com>
2021-11-18 10:39:37 -0300

    feat: menu navigation

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router'
+import Link, { LinkProps } from 'next/link'
+import { ReactElement, cloneElement } from 'react'
+
+type ActiveLinkProps = LinkProps & {
+  children: ReactElement
+  activeClassName: string
+}
+
+export const ActiveLink = ({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps) => {
+  const { asPath } = useRouter()
+  const className = asPath === rest.href ? activeClassName : ''
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
 
 export const Header = () => {
   return (
@@ -7,10 +8,12 @@ export const Header = () => {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig news logo" />
         <nav>
-          <a className={styles.active} href="">
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
