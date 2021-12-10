773531cf790970d8a2ea5981b6492a2504f77033
Saull Brandão <saull@outlook.com>
2021-12-10 09:20:08 -0300

    test: activelink component

--- src/components/ActiveLink/index.test.tsx
+++ src/components/ActiveLink/index.test.tsx
@@ -1,12 +1,22 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { ActiveLink } from '.'
 
-test('active link render correctly', () => {
-  const { debug } = render(
-    <ActiveLink href="/" activeClassName="active">
-      <a>Home</a>
-    </ActiveLink>,
-  )
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 
-  debug()
+useRouter.mockImplementation(() => {
+  return {
+    asPath: '/',
+  }
+})
+
+describe('ActiveLink component', () => {
+  it('adds active class if the link is currently active', () => {
+    render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>,
+    )
+
+    expect(screen.getByText('Home')).toHaveClass('active')
+  })
 })
